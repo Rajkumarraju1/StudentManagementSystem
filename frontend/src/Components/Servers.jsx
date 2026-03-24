7963a96f3c13237467befea6c73ead06124f2a71
Raj Kumar Raju <rajkumarraju60433@gmail.com>
2026-03-24 11:39:07 +0530

    Updated CORS for frontend

--- frontend/src/Components/Servers.jsx
+++ frontend/src/Components/Servers.jsx
@@ -1,28 +1,29 @@
 import axios from "axios"
 
-const BaseUrl="http://localhost:8080/Student";
-const desUrl="http://localhost:8080/designation"
 
-export const getStudent=()=>{
-   return axios.get(BaseUrl);
+const BASE_URL = "https://studentmanagementsystem-ozai.onrender.com/Student";
+const DES_URL = "https://studentmanagementsystem-ozai.onrender.com/designation";
+
+export const getStudent = () => {
+   return axios.get(BASE_URL);
 }
 
-export const saveStudent=(student)=>{
-    return axios.post(BaseUrl,student)
-}
-export const saveDesignation=()=>{
-    return axios.get(desUrl);
-}
- 
-
-export const deleteStudent=(id)=>{
-    return axios.delete(`${BaseUrl}/${id}`);
+export const saveStudent = (student) => {
+    return axios.post(BASE_URL, student);
 }
 
-export const updateStudent=(id,student)=>{
-    return axios.put(`${BaseUrl}/${id}`,student)
+export const saveDesignation = () => {
+    return axios.get(DES_URL);
 }
 
-export const getStudentById=(id)=>{
-    return axios.get(`${BaseUrl}/${id}`)
+export const deleteStudent = (id) => {
+    return axios.delete(`${BASE_URL}/${id}`);
+}
+
+export const updateStudent = (id, student) => {
+    return axios.put(`${BASE_URL}/${id}`, student);
+}
+
+export const getStudentById = (id) => {
+    return axios.get(`${BASE_URL}/${id}`);
 }
